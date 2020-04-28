2a87929faf857282784fe24349da013b68027f9a
OrkhanMusayev <mussayevorkhan@gmail.com>
2020-04-28 15:45:23 -0400

    Happy Birthday

---
+++ src/day_1/HappyBirthday.java
@@ -0,0 +1,15 @@
+package day_1;
+
+public class HappyBirthday {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("***************");
+        System.out.println("*    Happy    *");
+        System.out.println("*   Birthday  *");
+        System.out.println("*             *");
+        System.out.println("***************");
+
+    }
+}
