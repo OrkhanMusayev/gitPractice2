f1aee34f8b37138f38e9c3c4c9bc6a7aecc5e7bb
OrkhanMusayev <mussayevorkhan@gmail.com>
2020-04-28 15:26:31 -0400

    AddCommand

---
+++ src/day_1/AddCommand.java
@@ -0,0 +1,18 @@
+package day_1;
+
+public class AddCommand {
+
+    public static void main(String[] args){
+
+        // I will se you in gitHub
+
+
+        //I made change here when i created addCommand class
+
+
+
+
+
+
+    }
+}
